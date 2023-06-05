970fbc7c45a14263bb9fccf590522cee747441e8
ZA269VU <azza.mzah@esprit.tn>
2023-06-06 00:18:28 +0100

    Modifier ProjectController pour ajouter la fonctionnalité XYZ

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -1,4 +1,29 @@
 package com.esprit.examen.controllers;
 
+import com.esprit.examen.entities.CategorieProduit;
+import com.esprit.examen.services.ICategorieProduitService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Api(tags = "Gestion des categories Produit")
+@RequestMapping("/categorieProduit")
+@RequiredArgsConstructor
 public class ProjectController {
+    private final ICategorieProduitService categorieProduitService;
+
+    @GetMapping
+    public List<CategorieProduit> getCategorieProduit() {
+        List<CategorieProduit> list = categorieProduitService.retrieveAllCategorieProduits();
+        return list;
+    }
+
+    @GetMapping("/{categorieProduit-id}")
+    public CategorieProduit retrieveCategorieProduit(@PathVariable("categorieProduit-id") Long categorieProduitId) {
+        return categorieProduitService.retrieveCategorieProduit(categorieProduitId);
+    }
+
 }
